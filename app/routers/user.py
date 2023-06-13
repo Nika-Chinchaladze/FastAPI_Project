4b2f357cf2ea8ef5dc9e072784bd49d990f7c400
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2023-06-13 12:21:31 +0400

    user router added

---
+++ app/routers/user.py
@@ -0,0 +1,21 @@
+from fastapi import status, HTTPException, Depends, APIRouter
+
+from sqlalchemy.orm import Session
+from app import models, schemas
+from app.database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
+router = APIRouter(prefix="/users", tags=["User"])
+
+
+@router.get("/{id}", response_model=schemas.SendUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    my_user = db.query(models.Author).filter(models.Author.id == id).first()
+    if my_user:
+        return my_user
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id {id} - not exists!",
+        )
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 
+# Token Data Pydantic Model
 class TokenData(BaseModel):
     author_id: Optional[str] = None
+
+
+# User Pydantic Models
+class BaseUser(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class GetUser(BaseUser):
+    password: str
+
+
+class SendUser(BaseUser):
+    id: int
+
+    class Config:
+        orm_mode = True
